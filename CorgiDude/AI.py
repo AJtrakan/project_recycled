f333b438ea58bd49d338bdd194472f7fdf69be77
trakan tantee <87230169+AJtrakan@users.noreply.github.com>
2023-02-10 00:05:58 +0700

    Add files via upload

--- CorgiDude/AI.py
+++ CorgiDude/AI.py
@@ -7,7 +7,7 @@ from fpioa_manager import fm
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
-sensor.set_windowing((224, 224))
+#sensor.set_windowing((224, 224))
 sensor.set_vflip(1)
 
 lcd.init()
@@ -19,17 +19,23 @@ fm.register(19, fm.fpioa.UART1_RX, force=True)
 
 uart_A = UART(UART.UART1, 9600, 8, 1, 0, timeout=1, read_buf_len=4096)
 
-#img1 = image.Image("/sd/2.jpg")
 
-labels=['level1','level2','level3'] #number of labels should match the number of labels the model was trained with
+
+labels=['level1','level2','level3','level4'] #number of labels should match the number of labels the model was trained with
 task = kpu.load(0x300000) #change to "/sd/name_of_the_model_file.kmodel" if loading from SD card
-kpu.set_outputs(task, 0, 1, 1, 3) #the actual shape needs to match the last layer shape of your model
+kpu.set_outputs(task, 0, 1, 1, 4) #the actual shape needs to match the last layer shape of your model
 while(True):
     #kpu.memtest()
 
     img = sensor.snapshot()
-    #img = img.resize(224,224)
+    #img = image.Image("/sd/42.jpg")
 
+    img=img.draw_rectangle((0, 0, 130, 240), color = (0, 0, 0), thickness = 1, fill = True)
+    img=img.draw_rectangle((193, 0,360, 240), color = (0, 0, 0), thickness = 1, fill = True)
+
+    img = img.resize(224,224)
+
+    lcd.display(img)
     #img = img.rotation_corr(z_rotation=90.0) uncomment if need rotation correction - only present in full maixpy firmware
     a = img.pix_to_ai()
     fmap = kpu.forward(task, img)
@@ -41,7 +47,6 @@ while(True):
     print((pmax, labels[max_index].strip()))
     a = lcd.display(img)
     AA = (labels[max_index].strip())
-
 
 
 
@@ -58,9 +63,10 @@ while(True):
             print("level = 3")
             uart_A.write(b'L')
             time.sleep(3)
-        #elif AA == 'level4':
-            #print("level = 4")
-            #time.sleep(1)
+        elif AA == 'level4':
+            print("level = 4")
+            uart_A.write(b'N')
+            time.sleep(1)
 
-
+    time.sleep(5)
 
